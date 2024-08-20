71a53e9541af2e9b2722aa7ae9eb43300b0a1092
Gavin Brown <iamslowtrain@gmail.com>
2024-08-20 16:42:24 +0100

    replace the alert with a dismissable cookie banner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
-/*  function to send an email via emailjs */
+// waits for the html and css to load before running the JS
+window.onload = function() {
+
+//  function to send an email via emailjs 
 
 function sendMail(contactForm) {
     emailjs.send("gmail","4uxdesign", {
@@ -17,4 +20,27 @@ function sendMail(contactForm) {
     alert("Thank you. I will get back to you within 48hrs.");
     getElementById("project-form").reset();
     return false;
+}
+
+// JS for the cookie banner
+document.getElementById("cookieBanner").style.display = "none"; // hides the cookie banner
+    
+// function to set a cookie when the 'accept cookies' button is pressed
+document.getElementById("acceptCookies").addEventListener("click", function() {
+    console.log("Button clicked, cookie set");
+    document.cookie = "=cookiesAccepted=YES; expires=Thu, 31 Dec 3000 12:00:00 UTC; path=/"; // sets a cookie
+    document.getElementById("cookieBanner").style.display = "none";
+});
+    
+// function to check for a cookie
+function checkCookie() {
+    let cookieSet = document.cookie; // looks for a cookie
+    console.log("Cookie",cookieSet);
+    if (cookieSet == "") {
+        console.log("No cookie set");
+        document.getElementById("cookieBanner").style.display = "block"; // if no cookie show cookie banner
+    } 
+}
+    
+checkCookie();
 }
