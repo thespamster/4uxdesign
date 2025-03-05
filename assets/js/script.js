08638e78aa7f45206efaae4bcbdf7aaa573c76ef
Gavin Brown <iamslowtrain@gmail.com>
2025-03-05 18:44:03 +0000

    adjust text font of ko-fi alert banner

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,7 +70,7 @@ const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 const appendAlert = (message, type) => {
     const wrapper = document.createElement('div')
     wrapper.innerHTML = [
-        `<div class="alert alert-${type} alert-dismissible alert-style" role="alert">`,
+        `<div class="alert alert-${type} alert-dismissible alert-style roboto-condensed-300" role="alert">`,
         `   <div>${message}</div>`,
         '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
         '</div>'
